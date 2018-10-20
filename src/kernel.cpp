a0cf4fb1997c3c4b164c4bfae1e3fd118d092d5a
I am The Cheese <ch33s3codes@gmail.com>
2018-10-20 20:38:04 +0300

    Fix project structure, link files and create koskernel.bin

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,5 +1,19 @@
-void kernelMain()
+// We dont have access to the OS std I/O, so we need to leverage on the black/white
+// video card setting on memory 0xb8000.
+// The text screen video memory for colour monitors resides at 0xB8000, and for monochrome monitors it is at address 0xB0000
+
+void printf(char *str)
 {
-    printf("Hello World");
-    while(1);
+    unsigned short *VideoMemory = (unsigned short *)0xb8000;
+
+    for (int i = 0; str[i] != '\0'; ++i)
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+}
+
+// Define extern "C" to prevent G++ from changing this name when writing to the .o file
+extern "C" void kernelMain(void *multiboot_structure, unsigned int magicnumber)
+{
+    printf("Hello, Kotani OS");
+    while (1)
+        ;
 }
--- src/loader.s
+++ src/loader.s
@@ -1,11 +1,29 @@
+.set MAGIC, 0x1badb002              # Since bootloader doesn't recognize this as a kernel (due to lack of a metric number)  
+.set FLAGS, (1<<0 | 1<<1)           # Set some flags as well
+.set CHECKSUM, -(MAGIC + FLAGS)     # Set the checksum (to Negative of Metric Number - The Flags)
+
+.section .multiboot                 # Add the metric number to the .o file
+    .long MAGIC
+    .long FLAGS
+    .long CHECKSUM
+
+
 .section .text
 .extern kernelMain
 .global loader
 
 loader:
     mov $kernel_stack, %esp
+    push %eax
+    push %ebx
+    call kernelMain
 
-
+_stop:
+    # Sanity Check => Ensure infinite loop is indeed infinite :-)
+    cli
+    hlt
+    jmp _stop
 
 .section .bss
-kernel_stack;
+.space 2*1024*1024; # 2 MiB
+kernel_stack:
